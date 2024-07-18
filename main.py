6ef4342c6753b536e9e7122ae14887bb0ec5affe
DamirBagautdinov <damirbaga65@gmail.com>
2024-07-18 23:28:22 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st programm
+print(9 ** 0.5 * 5)
+#2nd programm
+print(9.99 > 9.98 and 1000!=100.1)
+#3rd programm
+print (1234//10)
+print (5678//10)
+print(123%100)
+print(567%100)
+print(23+67)
+#4th programm
+print (int(13.42))
+print(int(42.13))
+print((type(int(42.13))) == (type(int(13.42))) or (type(float(13.42))) == (type(float(42.42))))
